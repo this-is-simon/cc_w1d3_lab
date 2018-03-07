c2b4a97a58abb07f63cac54de49c41d14b3d7597
Alexhub88 <alexscott88@hotmail.co.uk>
2018-03-07 17:29:42 +0000

    Stuck on last one

--- my_functions.rb
+++ my_functions.rb
@@ -2,3 +2,23 @@ def add_array_lengths(array_1, array_2)
   sum_length = array_1.length + array_2.length
   return sum_length
 end
+
+def sum_array(array_1)
+  running_total = 0
+  for i in 0..array_1.length-1
+    running_total += array_1[i]
+  end
+  return running_total
+end
+
+def find_item(array_1, item)
+    for harry_potter_house in 0..array_1.length-1
+      if array_1[harry_potter_house] == item
+        return true
+      end
+    end
+    return false
+end
+
+def get_first_key
+end
